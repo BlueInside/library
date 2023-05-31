653a70be4eeb4be08262f37197a2ff0f9747e491
Karol <kpulawski13@gmail.com>
2023-05-31 18:07:45 +0100

    added func to remove books

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLibrary = [
+const myLibrary = [
   {
     title: "Lord of ice garden",
     author: "Jaroslaw grzedowicz",
@@ -25,32 +25,34 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
   }
 }
+
 const booksDisplay = document.querySelector(".book-display");
 function displayBooks() {
   booksDisplay.innerHTML = "";
   let index = 0;
   myLibrary.forEach((item) => {
     const book = document.createElement("div");
-    book.dataset.index = index;
     book.classList.add("book");
     book.innerHTML = `<div class="title">${item.title}</div>
     <div class="author">${item.author}</div>
     <div class="pages">PAGES: ${item.pages}</div>
-    <div class="read">READ - ${item.read}</div>`;
+    <div class="read">READ - ${item.read}</div>
+    <button data-index="${index}" class="removeBook">Remove me</button>`;
     booksDisplay.appendChild(book);
     index++;
   });
+  addIndexToButtons();
 }
 
 const btn = document.querySelector("#addBook");
 const form = document.querySelector(".hidden");
-function displayForm() {
+function showHideForm() {
   form.classList.toggle("addBook-form");
   form.classList.toggle("hidden");
   bookForm.reset();
 }
 
-btn.addEventListener("click", displayForm);
+btn.addEventListener("click", showHideForm);
 const bookForm = document.querySelector("#bookForm");
 bookForm.addEventListener("submit", submitForm);
 
@@ -63,10 +65,26 @@ function submitForm(e) {
   const b = new Book(title, author, pages, read);
   addBookToLibrary(b);
   displayBooks();
-  bookForm.reset();
+  showHideForm();
 }
-// take data from form
-//-- e.target[0-4].value and use it to create Book
-//---- just query select all inputs and use them to create book?
-// -- we need strings to create books ?
-// object and add it to myLibrary array
+
+function addIndexToButtons() {
+  let removeBtns = document.querySelectorAll(".removeBook");
+  removeBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      removeBookFromLibrary(e.target.getAttribute("data-index"));
+    });
+  });
+}
+function removeBookFromLibrary(removeIndex) {
+  myLibrary.splice(removeIndex, 1);
+  displayBooks();
+}
+//REMOVE BOOKS
+// when user clicks on button
+//-- Add event listeners to all buttons
+// remove me books should be removed
+// use data attribute on buttons to remove right element from
+// array ? use splice
+// from the array and display refreshed
+// return new array
