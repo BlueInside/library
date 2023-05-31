78caed5d3a750358b9e34e36342a5d9bfa9d31b0
Karol <kpulawski13@gmail.com>
2023-05-31 09:20:55 +0100

    created lib array Book obj and addBook func

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let myLibrary = [];
 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  if (book instanceof Book) {
+    myLibrary.push(book);
+  }
+}
